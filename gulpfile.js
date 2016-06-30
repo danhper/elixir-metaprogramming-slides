3d1f60fdbcec36c2dfd5a96c34158d269c96c274
Daniel Perez <daniel@claudetech.com>
2016-06-30 17:59:21 +0900

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,31 @@ var serveStatic   = require('serve-static')
 var livereload    = require('gulp-livereload')
 
 var PORT = process.env.PORT || 7050
+var BUILD_DIR = process.env.BUILD_DIR || './build';
 
 var dev = process.env.NODE_ENV !== 'production'
 
 gulp.task('jade', function () {
   return gulp.src('./src/index.jade')
     .pipe(jade({pretty: dev}))
-    .pipe(gulp.dest('./build'))
+    .pipe(gulp.dest(BUILD_DIR))
     .pipe(livereload())
 })
 
 gulp.task('stylus', function () {
   return gulp.src('./src/css/main.styl')
     .pipe(stylus({compress: !dev}))
-    .pipe(gulp.dest('./build/css'))
+    .pipe(gulp.dest(path.join(BUILD_DIR, 'css')))
     .pipe(livereload())
 })
 
-gulp.task('copy:images', function(arg) {
+gulp.task('copy:images', function (arg) {
   return gulp.src('./src/images/*')
-    .pipe(gulp.dest('./build/images'))
+    .pipe(gulp.dest(path.join(BUILD_DIR, 'images')))
     .pipe(livereload())
 })
 
-gulp.task('watch', function(arg) {
+gulp.task('watch', function (arg) {
   gulp.watch('./src/images/*', ['copy:images'])
   gulp.watch('./src/**/*.jade', ['jade'])
   gulp.watch('./src/snippets/*', ['jade'])
@@ -44,7 +45,7 @@ gulp.task('livereload', function () {
 
 gulp.task('connect', function () {
   var app = connect()
-  app.use(serveStatic(path.resolve('./build')))
+  app.use(serveStatic(path.resolve(BUILD_DIR)))
   http.createServer(app).listen(PORT, function () {
     console.log('Server listening at http://localhost:%s', PORT);
   })
